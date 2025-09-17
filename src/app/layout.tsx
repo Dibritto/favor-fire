7a526917c1b412a73b229f97cbc9abe935bef002
Edmilson Britto de Almeida dibritto <dimedebritto@gmail.com>
2025-09-17 10:33:15 +0000

    c3f6eb667c0186c5-s.p.woff2:1   Failed to load resource: the server respo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
-import { PT_Sans } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
-
-const ptSans = PT_Sans({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  variable: '--font-pt-sans',
-});
 
 export const metadata: Metadata = {
   title: 'Kindred Connect',
@@ -22,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" suppressHydrationWarning>
-      <body className={`${ptSans.variable} font-body antialiased`}>
+      <body className="font-body antialiased">
         <ThemeProvider storageKey="app-theme-mode">
           {children}
           <Toaster />
